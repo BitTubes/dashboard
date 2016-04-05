e3b59751a4700c60c72c7f432a10a60b97e0277a
Jörn Berkefeld <joern.berkefeld@gmail.com>
2016-04-05 18:55:21 +0200

    added Markdown / ng-showdown for video-configs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function() {
 	"use strict";
 	// var app = angular.module('btCms', [ 'filterElapsed', 'jbServices', 'ngRoute' ]);
-	angular.module('bittubes', ['jb.services', 'jb.notification', 'jb.i18n', 'ui.router','ui.bootstrap', 'ngMessages', 'ngAnimate', 'angular-storage' ])
+	angular.module('bittubes', ['jb.services', 'jb.notification', 'jb.i18n', 'ui.router','ui.bootstrap', 'ngMessages', 'ngAnimate', 'angular-storage', 'ng-showdown' ])
 
 	.config(function($animateProvider) {
 		$animateProvider.classNameFilter(/angular-animate/);
