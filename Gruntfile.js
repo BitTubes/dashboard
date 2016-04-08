df4a09390d27e2a05290e59a0a5b304565cd6785
Jörn Berkefeld <joern.berkefeld@gmail.com>
2016-04-08 12:04:53 +0200

    minor cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -412,9 +412,10 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'dashboardTempApp',
+          module: 'bt.dashboard',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
+
         },
         cwd: '<%= yeoman.app %>',
         src: [
@@ -522,10 +523,10 @@ module.exports = function (grunt) {
     ]);
   });
 
-  grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
-    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run(['serve:' + target]);
-  });
+  // grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
+  //   grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+  //   grunt.task.run(['serve:' + target]);
+  // });
 
   grunt.registerTask('test', [
     'clean:server',
