34ab6feba47c8d2f952d87cb2bbb1f2b2290e2c3
Jörn Berkefeld <joern.berkefeld@gmail.com>
2016-04-15 16:10:30 +0200

    reactivated prior test

--- app/common/btn-spinner.directive.spec.js
+++ app/common/btn-spinner.directive.spec.js
@@ -1,121 +1,121 @@
-// describe('Directive: btn-spinner', function() {
+describe('Directive: btn-spinner', function() {
 
-//   // load the directive's module
-//   beforeEach(module('bt.dashboard'));
+	// load the directive's module
+	beforeEach(module('bt.dashboard'));
 
-//   var element;
-//   var linker;
-//   var scope;
-//   var $compile;
-//   var $body;
-//   var html;
-//   var $rootScope;
-//   var $q;
+	var element;
+	var linker;
+	var scope;
+	var $compile;
+	var $body;
+	var html;
+	var $rootScope;
+	var $q;
 
-//   beforeEach(inject(function(_$rootScope_, _$compile_, $httpBackend, _$q_) {
-//     $rootScope = _$rootScope_;
-//     scope = $rootScope.$new();
-//     $compile = _$compile_;
-//     $q = _$q_;
-//     $httpBackend.expectGET('i18n/de.json').respond({});
-//     $body = angular.element('body');
-//     $body.empty();
-//     html = '<form name="testForm">' +
-//               '<button id="testbutton" btn-spinner="spinner" ng-disabled="{{disabled}}">Testbutton</button>' +
-//            '</form>';
-//     element = angular.element(html);
-//     linker = $compile(element);
-//     element = linker(scope);
-//   }));
+	beforeEach(inject(function(_$rootScope_, _$compile_, $httpBackend, _$q_) {
+		$rootScope = _$rootScope_;
+		scope = $rootScope.$new();
+		$compile = _$compile_;
+		$q = _$q_;
+		// $httpBackend.expectGET('i18n/de.json').respond({});
+		$body = angular.element('body');
+		$body.empty();
+		html = '<form name="testForm">' +
+							'<button id="testbutton" btn-spinner="spinner" ng-disabled="{{disabled}}">Testbutton</button>' +
+					 '</form>';
+		element = angular.element(html);
+		linker = $compile(element);
+		element = linker(scope);
+	}));
 
-//   it('It should prepend a spinner if a truthy value is bound to the directive', function() {
-//     scope.spinner = true;
+	it('It should prepend a spinner if a truthy value is bound to the directive', function() {
+		scope.spinner = true;
 
-//     scope.$digest();
+		scope.$digest();
 
-//     var firstChild = element.find('button').eq(0).children().eq(0);
+		var firstChild = element.find('button').eq(0).children().eq(0);
 
-//     expect(firstChild.hasClass('icons-spinner')).toBe(true);
-//   });
+		expect(firstChild.hasClass('icons-spinner')).toBe(true);
+	});
 
-//   it('It should remove the spinner if a falsy value is bound to the directive', function() {
-//     scope.spinner = true;
+	it('It should remove the spinner if a falsy value is bound to the directive', function() {
+		scope.spinner = true;
 
-//     scope.$digest();
+		scope.$digest();
 
-//     scope.spinner = false;
+		scope.spinner = false;
 
-//     scope.$digest();
+		scope.$digest();
 
-//     var children = element.find('button').eq(0).children();
+		var children = element.find('button').eq(0).children();
 
-//     expect(children.length).toBe(0);
-//   });
+		expect(children.length).toBe(0);
+	});
 
-//   it('It should show the spinner if a promise is bound to the directive', function() {
-//     scope.spinner = $q.defer().promise;
+	it('It should show the spinner if a promise is bound to the directive', function() {
+		scope.spinner = $q.defer().promise;
 
-//     scope.$digest();
+		scope.$digest();
 
-//     var firstChild = element.find('button').eq(0).children().eq(0);
+		var firstChild = element.find('button').eq(0).children().eq(0);
 
-//     expect(firstChild.hasClass('icons-spinner')).toBe(true);
-//   });
+		expect(firstChild.hasClass('icons-spinner')).toBe(true);
+	});
 
-//   it('It should remove the spinner if a promise is resolved', function() {
-//     var deferred = $q.defer();
-//     scope.spinner = deferred.promise;
+	it('It should remove the spinner if a promise is resolved', function() {
+		var deferred = $q.defer();
+		scope.spinner = deferred.promise;
 
-//     scope.$digest();
+		scope.$digest();
 
-//     var children = element.find('button').eq(0).children();
-//     expect(children.length).toBe(1);
+		var children = element.find('button').eq(0).children();
+		expect(children.length).toBe(1);
 
-//     deferred.resolve();
+		deferred.resolve();
 
-//     scope.$digest();
+		scope.$digest();
 
-//     children = element.find('button').eq(0).children();
-//     expect(children.length).toBe(0);
-//   });
+		children = element.find('button').eq(0).children();
+		expect(children.length).toBe(0);
+	});
 
-//   it('It should disable the button while running', function() {
-//     var button = element.find('button').eq(0);
-//     scope.spinner = true;
+	it('It should disable the button while running', function() {
+		var button = element.find('button').eq(0);
+		scope.spinner = true;
 
-//     scope.$digest();
+		scope.$digest();
 
-//     expect(button.prop('disabled')).toBe(true);
-//   });
+		expect(button.prop('disabled')).toBe(true);
+	});
 
-//   it('It should reevaluate ng-disabled after finishing', function() {
-//     var button = element.find('button').eq(0);
-//     scope.disabled = true;
+	it('It should reevaluate ng-disabled after finishing', function() {
+		var button = element.find('button').eq(0);
+		scope.disabled = true;
 
-//     scope.spinner = true;
+		scope.spinner = true;
 
-//     scope.$digest();
+		scope.$digest();
 
-//     expect(button.prop('disabled')).toBe(true);
+		expect(button.prop('disabled')).toBe(true);
 
-//     scope.spinner = false;
+		scope.spinner = false;
 
-//     scope.$digest();
+		scope.$digest();
 
-//     expect(button.prop('disabled')).toBe(true);
-//   });
+		expect(button.prop('disabled')).toBe(true);
+	});
 
-//   it('It should prepend only one spinner regardless how many times it is called', function() {
-//     var button = element.find('button').eq(0);
-//     scope.spinner = 'test_1';
+	it('It should prepend only one spinner regardless how many times it is called', function() {
+		var button = element.find('button').eq(0);
+		scope.spinner = 'test_1';
 
-//     scope.$digest();
+		scope.$digest();
 
-//     scope.spinner = 'test_2';
+		scope.spinner = 'test_2';
 
-//     scope.$digest();
+		scope.$digest();
 
-//     var children = element.find('button').eq(0).children();
-//     expect(children.length).toBe(1);
-//   });
-// });
+		var children = element.find('button').eq(0).children();
+		expect(children.length).toBe(1);
+	});
+});
