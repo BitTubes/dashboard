d4d8432a8e377c91f067575ef098e2d5121124ae
Jörn Berkefeld <joern.berkefeld@gmail.com>
2016-04-15 16:05:28 +0200

    added Karma-Coverage

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -64,7 +64,8 @@ module.exports = function(config) {
     // Which plugins to enable
     plugins: [
       'karma-phantomjs-launcher',
-      'karma-jasmine'
+      'karma-jasmine',
+      'karma-coverage',
     ],
 
     // Continuous Integration mode
@@ -83,5 +84,22 @@ module.exports = function(config) {
     // },
     // URL root prevent conflicts with the site root
     // urlRoot: '_karma_'
+
+
+    // coverage reporter generates the coverage
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'app/**/*.js': ['coverage']
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : '.tmp/coverage/'
+    }
   });
 };
