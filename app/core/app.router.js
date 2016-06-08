1e7b6fc2e7d5272c4186fa09339dc05040cc5fef
igor <ifr@fokus.fraunhofer.de>
2016-06-08 17:29:15 +0200

    fixes #19 - remove password max-length

--- app/core/app.router.js
+++ app/core/app.router.js
@@ -99,7 +99,7 @@
 		$rootScope.loginMax = 50;
 		$rootScope.loginMin = 5;
 		$rootScope.loginPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		$rootScope.passwordMax = 12;
+		$rootScope.passwordMax = 24;
 		$rootScope.passwordMin = 8;
 		$rootScope.passwordPattern = /^[.a-zA-Z0-9_-]*$/;
 
