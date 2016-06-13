caf1f4ff27e79313383be82078d8380841340073
igor <igor.fritzsch@bittubes.com>
2016-06-13 15:50:44 +0200

    adds google charts to admin/monitoring page

--- app/components/admin/monitoring/monitoring.js
+++ app/components/admin/monitoring/monitoring.js
@@ -11,6 +11,7 @@
 		// vm.add = add;
 		vm.days = 1;
 		vm.PINGS = null;
+		vm.myChartObject = {};
 		// vm.delete = del;
 		// vm.edit = edit;
 		// vm.viewAccount = viewAccount;
@@ -95,6 +96,69 @@
 			$scope.loadPromise = http.post($rootScope.uriApiAnalyzer + 'getMonitoring', { 'api': $rootScope.API, 'p': {'days': vm.days} })
 			.then(function(response) {
 				vm.PINGS = response.data;
+				vm.myChartObject.type = 'LineChart';
+				vm.myChartObject.data = {
+					'cols': [
+						{id: 'gsDateTime', label: 'Time of Day', type: 'date'},
+						{id: 'gsMaxResp', label: 'Max. Response Time', type: 'number'},
+						{id: 'gsMaxResp', label: 'Avg. Response Time', type: 'number'},
+						{id: 'gsMaxResp', label: 'Min. Response Time', type: 'number'},
+						{id: 'gsVal', label: 'Error Rate', type: 'number'}
+					],
+					'rows': []
+				};
+
+				angular.forEach(vm.PINGS, function(value, key) {
+					vm.myChartObject.data.rows.push(
+						{c: [
+							{v: new Date(value['ts']*1000)},
+							{v: value['response']['max']},
+							{v: value['response']['avg']},
+							{v: value['response']['min']},
+							{v: value['error_rate']}
+						]}
+					);
+				});
+
+				vm.myChartObject.options = {
+					pointSize: 5,
+					title: 'HTTP-CDN Monitoring last ' + vm.days + ' days',
+					focusTarget: 'category',
+					colors: [
+						'#ff9900', //orange
+						'#3366cc', // blue
+						'#109618', // green
+						'#dc3912' // red
+					],
+					legend: { position: 'top' },
+					vAxes: {
+						0: {title: 'Response Time (ms)', minValue: 0,  format: '#,### ms' },
+						1: {title: 'Error Rate (errors/call)', minValue: 0, maxValue: 0.05, format: '#,###.## %'}
+					},
+					series:{
+						0:{targetAxisIndex:0},
+						1:{targetAxisIndex:0},
+						2:{targetAxisIndex:0},
+						3:{targetAxisIndex:1}
+					},
+					formatters: {
+						date: [{
+							columnNum: 0,
+							pattern: 'HH:mm:ss dd:MM:yy'
+							// pattern: '#,###.### ms'
+						}]
+					}
+
+				};
+
+				// var _formatter = new window.google.visualization.NumberFormat({
+				// 		pattern:'#,###.### ms'
+				// 	}),
+				// 	_formatter2 = new window.google.visualization.NumberFormat({
+				// 			pattern:'#,###.## %'
+				// 	});
+				// _formatter.format(myChartObject, 1);
+
 			},
 			Auth.checkHttpStatus.bind(Auth));
 		}
