418b9392018de251c819686803ce9ff2742f78ab
Jörn Berkefeld <joern.berkefeld@gmail.com>
2016-06-14 11:08:27 +0200

    refactoring #23

--- app/components/admin/monitoring/monitoring.js
+++ app/components/admin/monitoring/monitoring.js
@@ -8,7 +8,7 @@
 	function MonitoringController(http, $scope, $rootScope, Auth, $locale) {
 		var vm = this;
 		vm.days = 1;
-		vm.drawChart = drawChart;
+		// vm.drawChart = drawChart;
 		vm.myChartObject = {};
 		vm.PINGS = null;
 		// keep the following in scope to ease copy-paste of the sorting
@@ -49,23 +49,25 @@
 				);
 			});
 
+			var numberPattern = '#' + $locale.NUMBER_FORMATS.DECIMAL_SEP + '###' + $locale.NUMBER_FORMATS.GROUP_SEP;
+			var msPattern = numberPattern + '### ms';
 			vm.myChartObject.formatters = {
 				number: [
 					{
 						columnNum: 1,
-						pattern: '#' + $locale.NUMBER_FORMATS.DECIMAL_SEP + '###' + $locale.NUMBER_FORMATS.GROUP_SEP + '### ms'
+						pattern: msPattern
 					},
 					{
 						columnNum: 2,
-						pattern: '#' + $locale.NUMBER_FORMATS.DECIMAL_SEP + '###' + $locale.NUMBER_FORMATS.GROUP_SEP + '### ms'
+						pattern: msPattern
 					},
 					{
 						columnNum: 3,
-						pattern: '#' + $locale.NUMBER_FORMATS.DECIMAL_SEP + '###' + $locale.NUMBER_FORMATS.GROUP_SEP + '### ms'
+						pattern: msPattern
 					},
 					{
 						columnNum: 4,
-						pattern: '#' + $locale.NUMBER_FORMATS.DECIMAL_SEP + '###' + $locale.NUMBER_FORMATS.GROUP_SEP + '## %'
+						pattern: numberPattern + '## %'
 					}]
 
 			};
@@ -82,8 +84,8 @@
 				],
 				legend: { position: 'top' },
 				vAxes: {
-					0: {title: 'Response Time (ms)', minValue: 0, format: '#' + $locale.NUMBER_FORMATS.DECIMAL_SEP + '### ms' },
-					1: {title: 'Error Rate (errors/call)', minValue: 0, maxValue: 0.05, format: '#' + $locale.NUMBER_FORMATS.DECIMAL_SEP + '###' + $locale.NUMBER_FORMATS.GROUP_SEP + '## %'}
+					0: {title: 'Response Time (ms)', minValue: 0, format: msPattern },
+					1: {title: 'Error Rate (errors/call)', minValue: 0, maxValue: 0.05, format: numberPattern + '## %'}
 				},
 				series: {
 					0: { targetAxisIndex: 0 },
@@ -102,7 +104,7 @@
 			$scope.loadPromise = http.post($rootScope.uriApiAnalyzer + 'getMonitoring', { 'api': $rootScope.API, 'p': {'days': vm.days} })
 			.then(function(response) {
 				vm.PINGS = response.data;
-				vm.drawChart(vm.PINGS);
+				drawChart(vm.PINGS);
 			},
 			Auth.checkHttpStatus.bind(Auth));
 		}
